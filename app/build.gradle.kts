380132baacd0aa8f42899393f6ff583bf04a2694
Yue1Ran <s160418002@gmail.com>
2024-10-08 13:12:13 +0700

    ach

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.android.volley:volley:1.2.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+
 }
