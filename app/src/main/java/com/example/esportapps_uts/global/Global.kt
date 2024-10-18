9da54d446aaf940481534806ea642099ff5311af
noventaaaa <107040216+noventaaaa@users.noreply.github.com>
2024-10-19 01:33:26 +0700

    add global n util

---
+++ app/src/main/java/com/example/esportapps_uts/global/Global.kt
@@ -0,0 +1,17 @@
+package com.example.esportapps_uts.global
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+
+class Global {
+    companion object {
+        val baseUrl = "http://192.168.1.3/esport/"
+        fun makeAlert(context: Context, title:String, message:String){
+            val alert = AlertDialog.Builder(context)
+            alert.setTitle(title)
+            alert.setMessage(message)
+            alert.setPositiveButton("OK") { _,_ ->}
+            alert.show()
+        }
+    }
+}
