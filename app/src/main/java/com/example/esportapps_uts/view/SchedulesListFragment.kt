b39c344f5bafeb3a411e84562e202a57d23eae16
noventaaaa <107040216+noventaaaa@users.noreply.github.com>
2024-10-20 18:43:27 +0700

    schedule

--- app/src/main/java/com/example/esportapps_uts/view/SchedulesListFragment.kt
+++ app/src/main/java/com/example/esportapps_uts/view/SchedulesListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.esportapps_uts.databinding.FragmentSchedulesListBinding
 import com.example.esportapps_uts.viewmodel.ListScheduleViewModel
@@ -12,42 +13,44 @@ import com.example.esportapps_uts.viewmodel.ListScheduleViewModel
 
 class SchedulesListFragment : Fragment() {
 
-//    private lateinit var viewModel: ListScheduleViewModel
-//    private val ScheduleListAdapter = SchedulesListAdapter(arrayListOf())
-//    private lateinit var binding: FragmentSchedulesListBinding
-//
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        binding = FragmentSchedulesListBinding.inflate(inflater,container, false)
-//        return binding.root
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//       // viewModel = ListScheduleViewModel(this).get(ListScheduleViewModel::class.java)
-//        viewModel.refresh()
-//        binding.recView.layoutManager = LinearLayoutManager(context)
-//        binding.recView.adapter = ScheduleListAdapter
-//        //observeViewModel()
-//
-//
-//    }
+    private lateinit var viewModel: ListScheduleViewModel
+    private val SchedulesListAdapter = SchedulesListAdapter(arrayListOf())
+    private lateinit var binding: FragmentSchedulesListBinding
 
-//    fun observeViewModel() {
-//        viewModel.LD.observe(viewLifecycleOwner, Observer { ScheduleListAdapter.updateScheduleList(it) })
-//        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
-//            if(it == true) {
-//                binding.recView.visibility = View.GONE
-//                binding.progressLoad.visibility = View.VISIBLE
-//            } else {
-//                binding.recView.visibility = View.VISIBLE
-//                binding.progressLoad.visibility = View.GONE
-//            }
-//        })
-//
-//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSchedulesListBinding.inflate(inflater,container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        viewModel = ListScheduleViewModel(this).get(ListScheduleViewModel::class.java)
+        viewModel.refresh()
+        binding.recView.layoutManager = LinearLayoutManager(context)
+        binding.recView.adapter = SchedulesListAdapter
+//        observeViewModel()
+
+
+    }
+
+    fun observeViewModel() {
+        viewModel.scheduleLD.observe(viewLifecycleOwner, Observer {
+            SchedulesListAdapter.updateScheduleList(it) })
+
+        viewModel.loadingLD.observe(viewLifecycleOwner, Observer {
+            if(it == true) {
+                binding.recView.visibility = View.GONE
+                binding.progressLoad.visibility = View.VISIBLE
+            } else {
+                binding.recView.visibility = View.VISIBLE
+                binding.progressLoad.visibility = View.GONE
+            }
+        })
+
+    }
 
 }
--- app/src/main/java/com/example/esportapps_uts/viewmodel/ListScheduleViewModel.kt
+++ app/src/main/java/com/example/esportapps_uts/viewmodel/ListScheduleViewModel.kt
@@ -46,5 +46,39 @@ class ListScheduleViewModel(application: Application):AndroidViewModel(applicati
 //        scheduleLoadErrorLD.value = false
 //        loadingLD.value = false
 
+        scheduleLD.value = arrayListOf(
+            Schedule("16055",
+                "Mobile Legend",
+                "Mobile Legends adalah salah satu game bergenre MOBA (Multiplayer Online Battle Arena) yang sangat populer di platform mobile. Dikembangkan oleh Moonton, game ini menghadirkan pertempuran 5v5 di mana pemain membentuk tim untuk menghancurkan markas lawan dengan mengendalikan berbagai hero yang memiliki kemampuan unik. Setiap hero dibagi dalam beberapa kategori seperti tank, marksman, mage, assassin, fighter, dan support, sehingga menciptakan strategi yang beragam dalam setiap pertandingan. Selain gameplay yang dinamis, Mobile Legends juga memiliki komunitas besar dan sering mengadakan turnamen eSports tingkat internasional, menjadikannya sebagai salah satu game mobile paling kompetitif dan diminati di dunia.",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai"),
+
+            Schedule("13312",
+                "PUBG",
+                "PUBG (PlayerUnknown's Battlegrounds) adalah game battle royale yang sangat populer di mana hingga 100 pemain bertarung dalam sebuah peta besar untuk menjadi orang terakhir yang bertahan hidup. Dikembangkan oleh PUBG Corporation, game ini menawarkan gameplay yang intens dan realistis, di mana pemain harus mengumpulkan senjata, perlengkapan, dan kendaraan sambil berusaha bertahan dari serangan pemain lain serta zona permainan yang semakin menyempit. PUBG dikenal karena elemen taktis dan strategi yang dibutuhkan untuk bertahan hidup, baik bermain solo, duo, maupun dalam tim. Dengan grafis yang memukau dan pengalaman yang mendebarkan, PUBG menjadi salah satu game battle royale terlaris dan sering diadakan dalam turnamen eSports skala global.",
+                "https://www.wallpaperflare.com/static/42/325/290/pubg-video-games-player-unknown-wallpaper.jpg",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai"),
+            Schedule("11204",
+                "Valorant",
+                "Valorant adalah game first-person shooter (FPS) taktis yang dikembangkan oleh Riot Games, dirilis pada tahun 2020. Game ini menggabungkan elemen penembakan cepat dengan kemampuan unik dari setiap karakter, yang disebut \"agent,\" menciptakan pengalaman gameplay yang strategis dan mendalam. Setiap agent memiliki peran dan keterampilan spesifik, seperti kemampuan untuk menembus dinding, meletakkan jebakan, atau memulihkan rekan satu tim, sehingga koordinasi dan komunikasi dalam tim menjadi kunci untuk memenangkan pertandingan. Dengan mode permainan berbasis putaran, di mana satu tim menyerang dan lainnya bertahan, Valorant memadukan elemen FPS klasik dengan inovasi yang memikat penggemar eSports dan komunitas gaming di seluruh dunia.",
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoqu9LGSC7JNonJWOsYndlPqRFLU-Qu6U7rg&s",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai",
+                "haihai")
+        )
+
+        scheduleLoadErrorLD.value = false
+        loadingLD.value = false
+
     }
 }
