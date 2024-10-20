5684f1491e2b6c97295ccd4171ee1dc69d557e4a
Yue1Ran <s160418002@student.ubaya.ac.id>
2024-10-21 02:41:35 +0700

    update ach adapter

--- app/src/main/java/com/example/esportapps_uts/view/AchievementsAdapter.kt
+++ app/src/main/java/com/example/esportapps_uts/view/AchievementsAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.esportapps_uts.view
 
-class AchievementsAdapter {
-}
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+//import com.example.esportapps_uts.databinding.AchievementsListItemBinding
+import com.example.esportapps_uts.model.Achievement
+import com.example.esportapps_uts.model.Game
+import com.example.esportapps_uts.util.loadImage
+
+//class AchievementsAdapter(val achList:ArrayList<Achievement>)
+ //   : RecyclerView.Adapter<AchievementsAdapter.AchievementsViewHolder>() {
+//    class AchievementsViewHolder(var binding: AchievementsListItemBinding)
+//        : RecyclerView.ViewHolder(binding.root)
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AchievementsViewHolder {
+//        val binding = Achievement.inflate(LayoutInflater.from(parent.context), parent, false)
+//        return AchievementsViewHolder(binding)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return achList.size
+//    }
+//
+//    override fun onBindViewHolder(holder: AchievementsViewHolder, position: Int) {
+//        holder.binding.txtName.text = achList[position].name
+//        holder.binding.txtAchievement.text = achList[position].
+//        holder.binding.imgGame.loadImage(achList[position].photoUrl , holder.binding.progressBarGame)
+//
+//        holder.binding.spinYear.setOnClickListener {
+//            val action = GamesListFragmentDirections.actionTeamList()
+//            Navigation.findNavController(it).navigate(action)
+//        }
+//    }
+//
+//    fun updateAchievementList(newAchievementList: ArrayList<Achievement>) {
+//        achList.clear()
+//        achList.addAll(newAchievementList)
+//        notifyDataSetChanged()
+//    }
+//}
