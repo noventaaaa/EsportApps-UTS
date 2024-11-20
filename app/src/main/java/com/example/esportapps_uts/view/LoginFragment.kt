45adc0a4893eb2f6f83677d02e255d162063d53b
Yue1Ran <s160418002@student.ubaya.ac.id>
2024-11-20 21:29:22 +0700

    2.0 login

---
+++ app/src/main/java/com/example/esportapps_uts/view/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.example.esportapps_uts.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/esportapps_uts/view/SignUpFragment.kt
@@ -0,0 +1,18 @@
+package com.example.esportapps_uts.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+
+class SignUpFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+
+
+}
